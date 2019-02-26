fdbbe550bfd269009414fa8f18e9183bb8b8c520
Neto Concon <concon.neto2@gmail.com>
2019-02-25 22:33:17 -0300

    add mediator model

---
+++ app/models/mediator.rb
@@ -0,0 +1,3 @@
+class Mediator < ApplicationRecord
+  belongs_to :user
+end
---
+++ test/models/mediator_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class MediatorTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
